b45c7b1a6ff18e61c3c797e7e64472aaa24be36c
Benjik <ben12pr@hotmail.com>
2022-10-25 11:34:57 +0200

    {Add} ConnexionScript
    Added a script to connect.

---
+++ Welcome/Assets/Scripts/Connexion/ConnexionScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System.IO;
+public class ConnexionScript : MonoBehaviour
+{
+    [Header("Username zone")]
+    [SerializeField] private TMP_InputField userName;
+    [Header("Game Data")]
+    [SerializeField] private GameDataScript gameData;
+
+
+    void Start()
+    {
+        //userName.text = gameData.playerName;
+
+    }
+
+    public void SetName()
+    {
+        gameData.playerName = userName.text;
+        Debug.Log(gameData.playerName);
+    }
+
+    public void resetName()
+    {
+        gameData.playerName = "";
+    }
+}
