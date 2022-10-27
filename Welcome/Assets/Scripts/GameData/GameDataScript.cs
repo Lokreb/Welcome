1446635da593236eb694f0d2f546271bfacb1bcd
Benjik <ben12pr@hotmail.com>
2022-10-27 15:01:21 +0200

    {Add} GameDataScript
    Contains all the player datas

---
+++ Welcome/Assets/Scripts/GameData/GameDataScript.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+//allows to add a Unity option to create the GameData object
+[CreateAssetMenu(menuName = "Create GameData")]
+
+public class GameDataScript : ScriptableObject
+{
+    //The data saved here is the player's name and score and key codes and the volume's value.
+    public string playerName;
+
+    public int score = 0;
+
+    public int volume = 100;
+    public int music = 100;
+
+    public List<int> idCible = new List<int>();
+
+    public int count = 0;
+
+    private void OnEnable()
+    {
+
+    }
+
+}
