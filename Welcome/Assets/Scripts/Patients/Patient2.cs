f1c19567e950f08e5a43b215c13d2803f6634c38
Lipoulpe <60692322+Lipoulpe@users.noreply.github.com>
2022-10-27 14:56:49 +0200

    {Add} Drag Effect on patient

--- Welcome/Assets/Scripts/Patients/Patient2.cs
+++ Welcome/Assets/Scripts/Patients/Patient2.cs
@@ -1,7 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Patient2 : MonoBehaviour
 {
+    public void Drag()
+    {
+        transform.position = Input.mousePosition;
+    }
 }
