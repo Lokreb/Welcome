e8f71335e5308986c630a728a9a090ac46345ce4
Benjik <ben12pr@hotmail.com>
2022-10-27 16:52:38 +0200

    {Add} PuzzleScript

---
+++ Welcome/Assets/Scripts/PuzzleGame/PuzzleScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PuzzleScript
+{
+    public int puzzle_ID;
+    public bool[] truePiecePosition = { false, false, false, false, false, false, false, false, false };
+    public bool completed = false;
+
+    public PuzzleScript(int truePosition)
+    {
+        truePiecePosition[truePosition] = true;
+    }
+}
