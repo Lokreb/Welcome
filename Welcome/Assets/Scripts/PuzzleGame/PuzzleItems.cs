b88d3af7df3529b941e9195d4205605d7745ffa9
Benjik <ben12pr@hotmail.com>
2022-10-28 10:24:35 +0200

    {Update} PuzzleItems
    Added scoring, checking the correct position of the puzzle piece, etc.

--- Welcome/Assets/Scripts/PuzzleGame/PuzzleItems.cs
+++ Welcome/Assets/Scripts/PuzzleGame/PuzzleItems.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PuzzleItems : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
@@ -45,20 +46,118 @@ public class PuzzleItems : MonoBehaviour, IPointerClickHandler, IBeginDragHandle
     public void OnEndDrag(PointerEventData eventData)
     {
         canvasGroup.alpha = 1.0f;
-        if (Slots.nameSelectedSlot == null)
+        Debug.Log("NameSelectedSlot ===== " + PuzzleSlots.nameSelectedSlot);
+        if (PuzzleSlots.nameSelectedSlot == null)
         {
-            rectTransform.anchoredPosition = new Vector2(0, 0);
-            canvasGroup.blocksRaycasts = true;
-            rectTransform.sizeDelta = new Vector2(200, 200);
+            StartingPosition();
         }
         else
         {
-
+            switch (PuzzleSlots.nameSelectedSlot)
+            {
+                case "Slot_A":
+                    if (_gameData.idCiblePuzzle[_gameData.count] == 0)
+                    {
+                        GoodPosition(PuzzleSlots.nameSelectedSlot);
+                        Destroy(eventData.pointerDrag);
+                    }
+                    else
+                        StartingPosition();
+                    break;
+                case "Slot_B":
+                    if (_gameData.idCiblePuzzle[_gameData.count] == 1)
+                    {
+                        GoodPosition(PuzzleSlots.nameSelectedSlot);
+                        Destroy(eventData.pointerDrag);
+                    }
+                    else
+                        StartingPosition();
+                    break;
+                case "Slot_C":
+                    if (_gameData.idCiblePuzzle[_gameData.count] == 2)
+                    {
+                        GoodPosition(PuzzleSlots.nameSelectedSlot);
+                        Destroy(eventData.pointerDrag);
+                    }
+                    else
+                        StartingPosition();
+                    break;
+                case "Slot_D":
+                    if (_gameData.idCiblePuzzle[_gameData.count] == 3)
+                    {
+                        GoodPosition(PuzzleSlots.nameSelectedSlot);
+                        Destroy(eventData.pointerDrag);
+                    }
+                    else
+                        StartingPosition();
+                    break;
+                case "Slot_E":
+                    if (_gameData.idCiblePuzzle[_gameData.count] == 4)
+                    {
+                        GoodPosition(PuzzleSlots.nameSelectedSlot);
+                        Destroy(eventData.pointerDrag);
+                    }
+                    else
+                        StartingPosition();
+                    break;
+                case "Slot_F":
+                    if (_gameData.idCiblePuzzle[_gameData.count] == 5)
+                    {
+                        GoodPosition(PuzzleSlots.nameSelectedSlot);
+                        Destroy(eventData.pointerDrag);
+                    }
+                    else
+                        StartingPosition();
+                    break;
+                case "Slot_G":
+                    if (_gameData.idCiblePuzzle[_gameData.count] == 6)
+                    {
+                        GoodPosition(PuzzleSlots.nameSelectedSlot);
+                        Destroy(eventData.pointerDrag);
+                    }
+                    else
+                        StartingPosition();
+                    break;
+                case "Slot_H":
+                    if (_gameData.idCiblePuzzle[_gameData.count] == 7)
+                    {
+                        GoodPosition(PuzzleSlots.nameSelectedSlot);
+                        Destroy(eventData.pointerDrag);
+                    }
+                    else
+                        StartingPosition();
+                    break;
+                case "Slot_I":
+                    if (_gameData.idCiblePuzzle[_gameData.count] == 8)
+                    {
+                        GoodPosition(PuzzleSlots.nameSelectedSlot);
+                        Destroy(eventData.pointerDrag);
+                    }
+                    else
+                        StartingPosition();
+                    break;
+            }
         }
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("OnPointerClick");
+    }
+
+    public void StartingPosition()
+    {
+        rectTransform.anchoredPosition = new Vector2(0, 0);
+        canvasGroup.blocksRaycasts = true;
+        rectTransform.sizeDelta = new Vector2(200, 200);
+    }
+    
+    public void GoodPosition(string name)
+    {
+        _gameData.scorePuzzleGame += 10;
+        _gameData.count++;
+        rectTransform.sizeDelta = new Vector2(260, 260);
+        GameObject go = GameObject.Find(name);
+        go.GetComponent<Image>().sprite = GetComponent<Image>().sprite;
     }
 }
