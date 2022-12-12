6a64144146b770c2df815b4e27d2532997e81baf
projetbuilder <85063467+Lokreb@users.noreply.github.com>
2022-12-12 17:30:40 +0100

    Integration des jeux saut et rythme à la branche

---
+++ Welcome/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float jumpForce;
+    float score;
+
+    [SerializeField]
+    bool isGrounded = false;
+    bool isAlive = true;
+
+    Rigidbody2D RB;
+
+    public Text scoreText;
+
+    private void Awake()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(isGrounded == true)
+            {
+                RB.AddForce(Vector2.up * jumpForce);
+                isGrounded = false;
+            }
+        }
+
+        if(isAlive)
+        {
+            score += Time.deltaTime * 4;
+            scoreText.text = "Score" + score.ToString("F");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("ground"))
+        {
+            if (isGrounded == false)
+            {
+                isGrounded = true;
+            }
+        }
+
+        if (collision.gameObject.CompareTag("spike"))
+        {
+            isAlive = false;
+            Time.timeScale = 0;
+        }
+    }
+}
