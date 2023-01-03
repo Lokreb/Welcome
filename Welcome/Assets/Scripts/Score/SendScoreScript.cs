37ea938453828f64770afde1bb8c8b7c12ba27e9
Benjik <ben12pr@hotmail.com>
2023-01-03 21:29:35 +0100

    {Update} SendScoreScript
    Coroutine only if we are in the menu scene. Added SendScore function

--- Welcome/Assets/Scripts/Score/SendScoreScript.cs
+++ Welcome/Assets/Scripts/Score/SendScoreScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SendScoreScript : MonoBehaviour
 {
@@ -15,9 +16,15 @@ public class SendScoreScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        MyBoard.AddScore(myData.playerName, myData.score);
+        if (SceneManager.GetActiveScene().name == "MenuScene")
+        {
+            StartCoroutine(routine: GetScores());
+        }
+    }
 
-        StartCoroutine(routine: GetScores());
+    public void SendScore()
+    {
+        MyBoard.AddScore(myData.playerName, myData.score);
     }
 
     private IEnumerator GetScores()
