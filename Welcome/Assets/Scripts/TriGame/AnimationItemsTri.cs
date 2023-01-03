2085f6ec890f8fff0554943aab7845eced89d0b3
Lipoulpe <60692322+Lipoulpe@users.noreply.github.com>
2023-01-03 10:40:35 +0100

    {Update} AnimationSpeedValue

--- Welcome/Assets/Scripts/TriGame/AnimationItemsTri.cs
+++ Welcome/Assets/Scripts/TriGame/AnimationItemsTri.cs
@@ -3,6 +3,7 @@ using DG.Tweening;
 
 public class AnimationItemsTri : MonoBehaviour
 {
+    float _vitesse = .18f;
     public void Apparition(float delai)
     {
         float offsetX = Random.Range(-5f, 5f);
@@ -11,9 +12,9 @@ public class AnimationItemsTri : MonoBehaviour
         Sequence sequence = DOTween.Sequence();
 
         sequence.SetDelay(delai);
-        sequence.Append(transform.DOMove(new Vector2(transform.position.x + offsetX, transform.position.y + 150f + offsetY),.25f).SetEase(Ease.Linear));
-        sequence.Join(transform.DORotate(new Vector3(0f,0f,180f), .25f).SetEase(Ease.Linear));
-        sequence.Append(transform.DOMove(new Vector2(transform.position.x + offsetX, transform.position.y + 300f + offsetY), .25f).SetEase(Ease.Linear));
-        sequence.Join(transform.DORotate(new Vector3(0f, 0f, 360f), .25f).SetEase(Ease.Linear));
+        sequence.Append(transform.DOMove(new Vector2(transform.position.x + offsetX, transform.position.y + 150f + offsetY),_vitesse).SetEase(Ease.Linear));
+        sequence.Join(transform.DORotate(new Vector3(0f,0f,180f), _vitesse).SetEase(Ease.Linear));
+        sequence.Append(transform.DOMove(new Vector2(transform.position.x + offsetX, transform.position.y + 300f + offsetY), _vitesse).SetEase(Ease.Linear));
+        sequence.Join(transform.DORotate(new Vector3(0f, 0f, 360f),_vitesse).SetEase(Ease.Linear));
     }
 }
