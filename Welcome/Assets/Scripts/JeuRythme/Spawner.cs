210ead183b8831595e202e37a4b9eb9abca33a12
Lokreb <fouacheletelliermarvin@gmail.com>
2023-01-06 17:07:18 +0100

    {Add} Formes qui tombent

--- Welcome/Assets/Scripts/JeuRythme/Spawner.cs
+++ Welcome/Assets/Scripts/JeuRythme/Spawner.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject cubePrefab; // reference to the cube prefab
+    public Formes cubePrefab; // reference to the cube prefab
     [SerializeField] List<Sprite> sprites = new List<Sprite>();
     public float spawnInterval = 1f; // interval between each spawn
     public int numCubes = 20; // number of cubes to spawn
+
+    [SerializeField] private GameObject zone;
 
     void Start()
     {
@@ -24,6 +26,7 @@ public class Spawner : MonoBehaviour
             // spawn a cube at the same position as the spawner
             Formes _formes = Instantiate(cubePrefab, transform.position, Quaternion.identity);
             _formes._image.sprite = sprites[UnityEngine.Random.Range(0, sprites.Count)];
+            _formes.transform.SetParent(zone.transform);
             // wait for "spawnInterval" seconds before spawning the next cube
             yield return new WaitForSeconds(spawnInterval);
         }
