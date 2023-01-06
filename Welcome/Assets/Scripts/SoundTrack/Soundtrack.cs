9756badb2c93eba61b49ac02c66559156d7cf55d
Benjik <ben12pr@hotmail.com>
2023-01-06 11:22:32 +0100

    {Update} Soundtrack Script
    Change bgm volume

--- Welcome/Assets/Scripts/SoundTrack/Soundtrack.cs
+++ Welcome/Assets/Scripts/SoundTrack/Soundtrack.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class Soundtrack : MonoBehaviour
 {
+    public static Soundtrack instance;
+
+    public AudioSource bgm;
+
+    public GameDataScript gameData;
+    // Start is called before the first frame update
+
+    private void Update()
+    {
+        bgm.volume = gameData.music / 100f;
+    }
+
     void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
