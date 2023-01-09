0c50366dc3b81e3c0e7c19f2c9dadaad3293b83e
Benjik <ben12pr@hotmail.com>
2023-01-09 22:03:55 +0100

    {Update} CubeCollider
    Added a ValidTarget function that checks if the target you press is valid or not. It can only be destroyed if it is valid.

--- Welcome/Assets/Scripts/JeuRythme/CubeCollider.cs
+++ Welcome/Assets/Scripts/JeuRythme/CubeCollider.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CubeCollider : MonoBehaviour
 {
     private bool _isIn;
- 
+    [SerializeField] private TrueValueManager _trueValueManager;
+
+    public void Start()
+    {
+        _trueValueManager = FindObjectOfType<TrueValueManager>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Ground")) {
-            //Debug.Log("Entre");
             _isIn = true;
         }
     }
@@ -23,7 +29,7 @@ public class CubeCollider : MonoBehaviour
     }
 
     private void PressToDestroy() {
-        if(Input.GetKeyDown(KeyCode.Space) && _isIn == true) {
+        if(Input.GetKeyDown(KeyCode.Space) && _isIn == true && ValidTarget()) {
             Destroy(gameObject);
             Hit();
         }
@@ -40,5 +46,15 @@ public class CubeCollider : MonoBehaviour
     private void Miss()
     {
         ScoreManager.Miss();
+    }
+
+    //A function that checks whether the target being pressed is valid or not.
+    private bool ValidTarget()
+    {
+        for(int i = 0; i < _trueValueManager._trueSpritesList.Count; i++)
+            if (GetComponentInParent<Image>().sprite.name == _trueValueManager._trueSpritesList[i].name)
+                return true;
+
+        return false;
     }
 }
