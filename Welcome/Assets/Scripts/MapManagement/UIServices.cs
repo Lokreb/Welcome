bf30e96781b183f4873f51e66e15a04e9bd02343
Lipoulpe <60692322+Lipoulpe@users.noreply.github.com>
2023-03-01 13:53:07 +0100

    {Add} Service Animations

--- Welcome/Assets/Scripts/MapManagement/UIServices.cs
+++ Welcome/Assets/Scripts/MapManagement/UIServices.cs
@@ -7,10 +7,14 @@ public class UIServices : MonoBehaviour
 {
     public TextMeshProUGUI NB_Patient;
     public TextMeshProUGUI Multiplicator;
+    public Sprite[] SpriteSheetsAnimation;
+    [SerializeField] private SpriteRenderer _ImageAnim;
+    int _indexAnimation = 0;
 
     public Service Service;
 
-    
+    int _nbPatient = 0;
+    float _multiplicator = 1f;
 
     private void Start()
     {
@@ -18,16 +22,55 @@ public class UIServices : MonoBehaviour
         Service.OnMultiplicatorChange += MultiplicatorChange;
     }
 
+    private void FixedUpdate()
+    {
+        if (_nbPatient <= 0) return;
+
+        if (GameStateManager.Instance.CurrentGameState == GameState.Paused) return;
+
+        WorkingAnimation();
+    }
+
     void PatientChange(int nb)
     {
+        _nbPatient = nb;
         NB_Patient.text = nb.ToString();
+
+        if (_nbPatient <= 0)
+        {
+            _ImageAnim.sprite = SpriteSheetsAnimation[0];
+            _waitASecond = 0f;
+        }
     }
 
     Color alpha0f = new Color(1f, 1f, 1f, 0f);
     Color alpha1f = new Color(1f, 1f, 1f, 1f);
     void MultiplicatorChange()
     {
+        _multiplicator = Service.VitesseMultiplicator;
         Multiplicator.color = Service.VitesseMultiplicator == 1 ? alpha0f : alpha1f;
-        Multiplicator.text = "x " + Service.VitesseMultiplicator.ToString();
+        Multiplicator.text = "x " + _multiplicator.ToString();
+    }
+
+    float _frameRateAnimation = 0f;
+    float _waitASecond = 0f;
+    void WorkingAnimation()
+    {
+        if(_waitASecond<=40f)
+        {
+            _waitASecond++;
+            return;
+        }
+
+        _frameRateAnimation += _multiplicator;
+
+        
+        if(_frameRateAnimation >= 4f)
+        {
+            _frameRateAnimation = 0f;
+            _indexAnimation = (_indexAnimation + 1) % SpriteSheetsAnimation.Length;
+            _ImageAnim.sprite = SpriteSheetsAnimation[_indexAnimation % SpriteSheetsAnimation.Length];
+        }
+
     }
 }
